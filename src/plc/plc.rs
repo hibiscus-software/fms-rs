c4f86dc77f40c56b1004a8b6a18b6c5b78d2582b
Cody Wellman <cody@codexmicro.systems>
2024-03-13 18:32:44 +0000

    Async plc testing

---
+++ src/plc/plc.rs
@@ -0,0 +1,23 @@
+// Copyright (C) 2024 Hibiscus Software. All rights reserved. This
+// work is licensed under the terms of the MIT license which can be
+// found in the root directory of this project.
+
+use super::plc_inputs::PLCInputs;
+
+pub struct PLC {
+    inputs: PLCInputs,
+}
+
+impl PLC {
+    #[must_use]
+    pub const fn new(inputs: PLCInputs) -> Self {
+        return Self {
+            inputs
+        }
+    }
+
+    /// Returns the state of the field e-stop button.
+    pub async fn get_field_estop(&mut self) -> bool {
+        return !self.inputs.field_estop;
+    }
+}
---
+++ src/plc/plc_inputs.rs
@@ -0,0 +1,7 @@
+// Copyright (C) 2024 Hibiscus Software. All rights reserved. This
+// work is licensed under the terms of the MIT license which can be
+// found in the root directory of this project.
+
+pub struct PLCInputs {
+    pub field_estop: bool,
+}
